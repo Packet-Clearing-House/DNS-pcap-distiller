4177c3f1fe3f3732901f8e1f2b8504f7ea965ce5
James Mitchell <james@pch.net>
2018-10-09 09:50:35 -0700

    WEB-1158 brought in packet distillation code

--- src/main/java/net/pch/dns/pcap/distiller/Application.java
+++ src/main/java/net/pch/dns/pcap/distiller/Application.java
@@ -2,9 +2,16 @@ package net.pch.dns.pcap.distiller;
 
 import jpcap.JpcapCaptor;
 import jpcap.NetworkInterface;
+import jpcap.packet.IPPacket;
 import jpcap.packet.Packet;
+import jpcap.packet.TCPPacket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.xbill.DNS.*;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
 
 @SpringBootApplication
 public class Application {
@@ -32,7 +39,47 @@ public class Application {
 
         int forever = -1;
         captor.loopPacket(forever, (Packet packet) -> {
-            System.out.println("received packet");
+            byte[] data = null;
+            try {
+                data = packet.data;
+                System.err.printf("new packet %d bytes\n", data.length);
+                if (data.length < 10)
+                    return;
+
+                int proto = 0;
+                if (packet instanceof TCPPacket) {
+                    data = Arrays.copyOfRange(data, 2, data.length);
+                    proto = 1;
+                }
+
+                //System.err.println(new String(Base64.getEncoder().encode(data)));
+
+                Message message = new Message(data);
+                Header header = message.getHeader();
+                int opCode = header.getOpcode();
+                String srcIp = ((IPPacket) packet).src_ip.getHostAddress();
+                String dstIp = ((IPPacket) packet).dst_ip.getHostAddress();
+
+                Record[] records = message.getSectionArray(Section.QUESTION);
+                if (records.length > 0) {
+                    String zoneName = records[0].getName().toString().toLowerCase();
+                    int type = records[0].getType();
+                    if (header.getFlag(Flags.QR) == false) {
+                        System.out.format("Q %s %s %d %d %d %s %d\n", srcIp, dstIp, proto, opCode, type, zoneName, data.length);
+                    } else {    // if response grab response code and reverse src/dst IPs
+                        int rCode = header.getRcode();
+                        System.out.format("R %s %s %d %d %d %s %d %d\n", dstIp, srcIp, proto, opCode, type, zoneName, data.length, rCode);
+                    }
+                    System.out.flush();
+                }
+            } catch (WireParseException ex) {
+                if (ex.getMessage().contains("compression")) {
+                    ex.printStackTrace(System.err);
+                    System.err.println(new String(Base64.getEncoder().encode(data)));
+                }
+            } catch (IOException e) {
+                System.err.println("io exceptions!" + e);
+            }
         });
 
         captor.close();
